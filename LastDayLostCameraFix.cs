4c5ca96dd69935b8bee666033450b1d534edf0c0
artkopt <artkopt@mail.ru>
2025-03-01 00:07:30 +0300

    1 version of the bug fix

--- LastDayLostCameraFix.cs
+++ LastDayLostCameraFix.cs
@@ -1,69 +1,168 @@
 using HarmonyLib;
-using Unity.Mathematics;
 using UnityEngine;
-using Zorro.Settings;
+using Zorro.Core;
+using Photon.Pun;
 
-// ExampleCWPlugin is mostly an example of how to use the modding API, not an actual serious mod.
-// It adds a setting to the Mods settings page, which is a slider from 0 to 100.
-// It then edits the Flashlight.Update method to prevent the battery of the flashlight
-// from falling below that setting value.
-
-namespace DisableDisappearanceItemsInBell;
-
-// The first argument is the GUID for this mod. This must be globally unique across all mods.
-// Consider prefixing your name/etc. to the GUID. (or generate an actual GUID)
-
-// vanillaCompatible: False means that this mod affects anything related to the multiplayer aspect of the game - for
-// example, adjusting sprint regen, battery life, changing monster behavior, or anything similar or more major. Most
-// mods leave this as false. 
-// True means that this mod only affects this client. For example, a mod that changes the folder your clips are saved at.
-// A good rule of thumb is: If someone else can tell you're using this mod, you must set vanillaCompatible to false.
-// If you set this to true, and it should be false, your mod may be removed/banned from the workshop.
-[ContentWarningPlugin("DisableDisappearanceItemsInBell", "0.1", vanillaCompatible: false)]
-public class DisableDisappearanceItemsInBell
+namespace LastDayLostCameraFix
 {
-    static DisableDisappearanceItemsInBell()
+    [ContentWarningPlugin("LastDayLostCameraFix", "1.0", vanillaCompatible: false)]
+    public class LastDayLostCameraFix
     {
-        // Static constructors of types marked with ContentWarningPluginAttribute are automatically invoked on load.
-        // Register callbacks, construct stuff, etc. here.
-        Debug.Log("Hello from DisableDisappearanceItemsInBell! This is called on plugin load");
-        // Adding the [ContentWarningSetting] attribute to a setting class is basically the same as:
-        // GameHandler.Instance.SettingsHandler.AddSetting(new ExampleSetting());
+        static LastDayLostCameraFix()
+        {
+            LogFile.LogMessage("Мод LastDayLostCameraFix запущен");
+
+            try
+            {
+                LogFile.LogMessage("Попытка применения патчей");
+
+                var harmony = new Harmony("com.artkopt.LastDayLostCameraFix");
+                harmony.PatchAll();
+
+                LogFile.LogMessage("Патчи применены.\n");
+            }
+            catch (Exception ex)
+            {
+                LogFile.LogMessage($"Ошибка при применении патчей: {ex.Message}");
+            }
+        }
+
+        [HarmonyPatch(typeof(SurfaceNetworkHandler))]
+        public static class Patch_InitSurface
+        {
+            [HarmonyPatch(nameof(SurfaceNetworkHandler.InitSurface))]
+            [HarmonyPrefix]
+            static bool Prefix(SurfaceNetworkHandler __instance)
+            {
+                LogFile.LogMessage("Патч InitSurface вызван");
+
+                LogFile.LogMessage("Инициализация поверхности");
+                Debug.Log("Initializing Surface");
+                __instance.m_View = __instance.GetComponent<PhotonView>();
+                LogFile.LogMessage($"PhotonView получен: {__instance.m_View != null}");
+
+                __instance.m_SteamLobby = MainMenuHandler.SteamLobbyHandler;
+                LogFile.LogMessage($"SteamLobbyHandler получен: {__instance.m_SteamLobby != null}");
+
+                if (SurfaceNetworkHandler.RoomStats == null)
+                {
+                    LogFile.LogMessage("RoomStatus равен null, создавая новый экземпляр.");
+                    SurfaceNetworkHandler.RoomStats = new RoomStatsHolder(__instance, SingletonAsset<BigNumbers>.Instance.StartMoney, BigNumbers.GetQuota(0), 3);
+                    LogFile.LogMessage($"RoomStats создан. StartMoney: {SingletonAsset<BigNumbers>.Instance.StartMoney}, Quota: {BigNumbers.GetQuota(0)}");
+
+                    if (PhotonNetwork.IsMasterClient)
+                    {
+                        LogFile.LogMessage("Являемся MasterClient.");
+                        PhotonNetwork.CurrentRoom.IsOpen = true;
+                        PhotonNetwork.CurrentRoom.IsVisible = true;
+                        LogFile.LogMessage($"Комната открыта: {PhotonNetwork.CurrentRoom.IsOpen}, Комната видна: {PhotonNetwork.CurrentRoom.IsVisible}");
+                        PhotonGameLobbyHandler.Instance.SetCurrentObjective(new InviteFriendsObjective());
+                        LogFile.LogMessage("Установлена цель: InviteFriendsObjective.");
+                        __instance.CheckSave();
+                        LogFile.LogMessage("Вызван CheckSave.");
+                    }
+                    else
+                    {
+                        LogFile.LogMessage("Не являемся MasterClient, обновляем RoomProperties.");
+                        __instance.OnRoomPropertiesUpdate(PhotonNetwork.CurrentRoom.CustomProperties);
+                    }
+
+                    if (SurfaceNetworkHandler.RoomStats.CurrentDay <= 1)
+                    {
+                        LogFile.LogMessage($"Первый день или меньше (CurrentDay: {SurfaceNetworkHandler.RoomStats.CurrentDay}).");
+                        __instance.firstDay = true;
+                        string localizedString = LocalizationKeys.GetLocalizedString(LocalizationKeys.Keys.HelmetWelcome);
+                        HelmetText.Instance.SetHelmetText(localizedString, 3f);
+                        Debug.Log("NEW RUN!");
+                        if (PhotonNetwork.IsMasterClient)
+                        {
+                            LogFile.LogMessage("Являемся MasterClient, вызываем SpawnSurfacePickups.");
+                            __instance.SpawnSurfacePickups();
+                        }
+                    }
+                    else if (PhotonNetwork.IsMasterClient)
+                    {
+                        LogFile.LogMessage("Не первый день и являемся MasterClient, спавним VideoCamera.");
+                        __instance.m_VideoCameraSpawner.SpawnMe(force: true);
+                    }
+
+                    if (__instance.m_SteamLobby != null)
+                    {
+                        LogFile.LogMessage("SteamLobby существует, открываем лобби.");
+                        __instance.m_SteamLobby.OpenLobby();
+                    }
+
+                    SpawnHandler.Instance.SpawnLocalPlayer(Spawns.House);
+                    LogFile.LogMessage("Спавним локального игрока в доме.");
+                }
+                else
+                {
+                    LogFile.LogMessage("RoomStats не null.");
+                    Debug.Log("Should do next day here but waiting for upload?");
+                    if (TimeOfDayHandler.TimeOfDay == TimeOfDay.Evening)
+                    {
+                        LogFile.LogMessage("Время суток - вечер.");
+                        RichPresenceHandler.SetPresenceState(RichPresenceState.Status_AtHouse);
+                        if (PhotonNetwork.IsMasterClient)
+                        {
+                            LogFile.LogMessage("Являемся MasterClient, проверяем наличие камеры.");
+                            SurfaceNetworkHandler.ReturnedFromLostWorldWithCamera = __instance.CheckIfCameraIsPresent(includeBrokencamera: true);
+                            LogFile.LogMessage($"Наличие камеры после возвращения: {SurfaceNetworkHandler.ReturnedFromLostWorldWithCamera}");
+
+                            if (!SurfaceNetworkHandler.ReturnedFromLostWorldWithCamera)
+                            {
+                                LogFile.LogMessage("Камера отсутствует, сбрасываем апгрейды.");
+                                SurfaceNetworkHandler.RoomStats.ResetCameraUpgrades();
+                                if (PhotonNetwork.IsMasterClient)
+                                {
+                                    LogFile.LogMessage("Являемся MasterClient, устанавливаем цель GoToBedFailedObjective.");
+                                    PhotonGameLobbyHandler.Instance.SetCurrentObjective(new GoToBedFailedObjective());
+                                    if (SurfaceNetworkHandler.RoomStats.IsQuotaDay && !SurfaceNetworkHandler.RoomStats.CalculateIfReachedQuota())
+                                    {
+                                        LogFile.LogMessage("Сегодня день квоты и квота не выполнена, вызываем NextDay.");
+                                        //__instance.NextDay();
+                                    }
+                                }
+                            }
+                            else if (PhotonNetwork.IsMasterClient)
+                            {
+                                LogFile.LogMessage("Камера присутствует, устанавливаем цель ExtractVideoObjective.");
+                                PhotonGameLobbyHandler.Instance.SetCurrentObjective(new ExtractVideoObjective());
+                            }
+
+                            if (!__instance.m_FailedQuota)
+                            {
+                                LogFile.LogMessage("Квота не провалена, проверяем наличие счёта из больницы.");
+                                __instance.CheckForHospitalBill();
+                            }
+                        }
+
+                        if (!Player.justDied && !__instance.m_FailedQuota)
+                        {
+                            LogFile.LogMessage("Игрок не умер и квота не провалена, спавним в DiveBell.");
+                            SpawnHandler.Instance.SpawnLocalPlayer(Spawns.DiveBell);
+                        }
+                    }
+                }
+
+                if (!__instance.m_FailedQuota)
+                {
+                    LogFile.LogMessage("Квота не провалена, инициализируем ShopHandler.");
+                    __instance.ShopHandler.InitShopHandler();
+                }
+
+                return false;
+            }
+        }
+
+        // Логирование для отладки
+        public class LogFile
+        {
+            public static void LogMessage(string message)
+            {
+                string logFilePath = @"D:\\Mod_for_ContentWarning\\Mods\\Disable_Disappearance_Camera_In_Bell\\log.txt";
+                System.IO.File.AppendAllText(logFilePath, $"[{System.DateTime.Now}]: {message}\n");
+            }
+        }
     }
-}
-
-// Harmony patches are automatically applied by the vanilla modloader.
-// If 0Harmony.dll is already loaded by BepInEx, then BepInEx's harmony will be used instead.
-[HarmonyPatch(typeof(Flashlight))]
-public class FlashlightPatches
-{
-    private static ExampleSetting? exampleSetting;
-
-    // Make sure the name matches when using a string in HarmonyPatch(). A good way to do so is by using nameof().
-    [HarmonyPatch(nameof(Flashlight.Update))]
-    [HarmonyPrefix]
-    private static bool UpdatePrefix(Flashlight __instance)
-    {
-        exampleSetting ??= GameHandler.Instance.SettingsHandler.GetSetting<ExampleSetting>();
-        // m_batteryEntry is public due to use of a publicizer
-        var bat = __instance.m_batteryEntry;
-        bat.m_charge = Mathf.Max(bat.m_charge, bat.m_maxCharge * (exampleSetting.Value / 100));
-
-        return true;
-    }
-}
-
-// Don't forget to inherit from IExposedSetting too!
-[ContentWarningSetting]
-public class ExampleSetting : FloatSetting, IExposedSetting
-{
-    public override void ApplyValue() => Debug.Log($"omg, mod setting changed to {Value}");
-
-    protected override float GetDefaultValue() => 100;
-    protected override float2 GetMinMaxValue() => new(0, 100);
-
-    // Prefer using the Mods category
-    public SettingCategory GetSettingCategory() => SettingCategory.Mods;
-
-    public string GetDisplayName() => "Example mod setting";
 }
